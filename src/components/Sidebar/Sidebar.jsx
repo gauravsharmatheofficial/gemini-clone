85984f20371990a92c028c3e3f268e6afbd6c483
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2024-12-11 21:34:57 +0530

    initial commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./sidebar.css";
+import { assets } from "../../assets/assets.js";
+function Sidebar() {
+  const [extended, setExtended] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended(!extended)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            {extended ? <p>What is react ...</p> : null}
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Setting</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
